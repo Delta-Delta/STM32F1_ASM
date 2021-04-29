951c8434f18166e9d4b00bb74d7aae2fa4d86aa2
Delta-Delta <cxf_official_mail@163.com>
2021-04-29 17:25:15 +0800

    串口初始化、发送完成

--- STM32_M3_ASM/Src/main.s
+++ STM32_M3_ASM/Src/main.s
@@ -1,6 +1,4 @@
-
-
-LED0 EQU 0x422101a0 
+LED0 			EQU			0x422101a0 
 RCC 			EQU			0x40021000
 RCC_APB2ENR 	EQU         (RCC+0x18)          ;0x40021018
 GPIOC			EQU 		0x40011000
@@ -9,12 +7,19 @@ GPIOC_BRR		EQU			(GPIOC+0x14)		;复位0
 GPIOC_BSRR		EQU			(GPIOC+0x10)		;置位1
 
 
+
+
+
+
 Stack_Size      EQU     0x64
 
+
                 AREA    STACK, NOINIT, READWRITE, ALIGN=3
+					
+				
+				
 Stack_Mem       SPACE   Stack_Size
 __initial_sp
-
 
 
 
@@ -34,6 +39,8 @@ __Vectors       DCD     __initial_sp               ; Top of Stack
                 PRESERVE8
                     
                 ENTRY
+				
+				
 Reset_Handler 
                 BL LEDC13_Init
 MainLoop        BL LED_ON
@@ -44,9 +51,12 @@ MainLoop        BL LED_ON
                 B MainLoop
              
 
-				
+		
 LEDC13_Init
 				PUSH {R0,R1, LR}
+				
+				EXTERN USART1_Init	
+				EXTERN USART1_SendByte
                 
                 LDR R1,=RCC_APB2ENR	 ;取寄存器地址
 				LDR R0,[R1]			;读取要操作的寄存器（内存）数据到寄存器R0
@@ -60,8 +70,15 @@ LEDC13_Init
 				ORR R0,R0,#0x300000
 				STR R0,[R1]
              
+				BL USART1_Init
+				
+				mov    r0, #'H' 
+				bl     USART1_SendByte
+			 
                 POP {R0,R1,PC}
-
+				
+				
+				
              
 LED_ON
                 PUSH {R0,R1, LR}    
---
+++ STM32_M3_ASM/Src/usart1.s
@@ -0,0 +1,67 @@
+GPIOA    		EQU			0x40010800
+GPIOA_CRH		EQU		    (GPIOA+0x04)
+GPIOA_ODR		EQU			(GPIOA+0x0C)
+RCC 			EQU			0x40021000
+RCC_APB2ENR 	EQU         (RCC+0x18)          ;0x40021018
+USART1          EQU 		0x40013800
+USART1_CR1		EQU			(USART1+0x0C)
+USART1_BRR		EQU			(USART1+0x08)
+USART1_DR       EQU			(USART1+0x04)		
+USART1_SR       EQU			(USART1+0x00)	
+
+
+
+				AREA USART, CODE, READONLY
+				EXPORT USART1_Init   ;标号前面添加 EXPORT 使外部文件可以调用标号内容
+				EXPORT USART1_SendByte
+					
+USART1_Init
+				PUSH {R0,R1,R2, LR}
+                
+                LDR R1,=RCC_APB2ENR	 ;取寄存器地址
+				LDR R0,[R1]			;读取要操作的寄存器（内存）数据到寄存器R0
+				MOV R2,#0x4004
+				ORR R0,R0,R2		
+				STR R0,[R1]			;将r0寄存器的值，传送到地址值为r1的（存储器）内存中
+             
+				
+				
+				LDR R1,=GPIOA_CRH
+				LDR R0,[R1]
+				ORR R0,R0,#0x4B0  ;设置PA9   50MHz速率 输出  PA10 输入模式  
+				STR R0,[R1]
+             
+				
+				
+				LDR R1,=USART1_CR1
+				LDR R0,[R1]
+				MOV R0,#0x340C
+				STR R0,[R1]
+				
+				;定义波特率为9600
+				LDR R1,=USART1_BRR
+				LDR R0,[R1]
+				MOV R0,#0x341
+				STR R0,[R1]
+			 
+                POP {R0,R1,R2,PC}
+
+
+USART1_SendByte
+				PUSH   {R0 - R3} 
+				LDR    R2, =USART1_DR   
+				STR    R0, [R2] 
+b1 
+				LDR    R2, =USART1_SR  
+				LDR    R2, [R2] 
+				TST    r2, #0x40 
+				BEQ    b1 
+				;发送完成(Transmission complete)等待 
+				POP    {r0 - r3} 
+				BX     lr 
+
+
+
+
+
+				END
